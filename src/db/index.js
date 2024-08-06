107651bd68782b7b7af2f306ec538e7e1c1058d5
Narendra <nskathayat77@gmail.com>
2024-08-06 12:22:18 +0530

    last

--- src/db/index.js
+++ src/db/index.js
@@ -4,8 +4,8 @@ import { dbName } from "../constant.js";
 const connectDB=async()=>{
     try {
         const mongourl=process.env.url
-        const mongooseconnection=await mongoose.connect(mongourl)
-        console.log(mongooseconnection)
+        const mongooseconnection=await mongoose.connect(`${mongourl}/${dbName}`)
+        // console.log(mongooseconnection.owner)
      
 
         
